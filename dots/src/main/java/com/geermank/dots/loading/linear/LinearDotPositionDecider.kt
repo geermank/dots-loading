d5d51214a25617565d1732175a31fe19ecdbe2df
German Krajcar <german.krajcar@mercadolibre.com>
2021-08-22 21:26:01 -0300

    Changes to linear loading

--- dots/src/main/java/com/geermank/dots/loading/linear/LinearDotPositionDecider.kt
+++ dots/src/main/java/com/geermank/dots/loading/linear/LinearDotPositionDecider.kt
@@ -7,7 +7,6 @@ import com.geermank.dots.utils.Coordinates
 
 internal class LinearDotPositionDecider : DotPositionDecider {
 
-
     override fun getPosition(dotIndex: Int, dot: Dot, dotLoadingSpecs: DotLoadingSpecs): Coordinates {
         return Coordinates(0.0, (dotLoadingSpecs.getContainerHeightInPixels() / 2.0) - (dot.getDiameter() / 2.0))
     }
--- dots/src/main/java/com/geermank/dots/loading/linear/LinearDotsAnimation.kt
+++ dots/src/main/java/com/geermank/dots/loading/linear/LinearDotsAnimation.kt
@@ -1,25 +1,33 @@
 package com.geermank.dots.loading.linear
 
 import android.animation.ValueAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
 import com.geermank.dots.dot.Dot
 import com.geermank.dots.loading.DotsAnimation
 import com.geermank.dots.loading.view.DotLoading
 
+private const val MARGIN_BETWEEN_DOTS_FACTOR = 1.5f
+
 internal class LinearDotsAnimation : DotsAnimation {
 
     override fun animateDot(container: DotLoading, dot: Dot, dotIndex: Int) {
-        val startPositionX = 0f + dotIndex * dot.getDiameter()
-        val endPositionX = container.getSizeInPixels().width - (startPositionX + dot.getDiameter())
+        val startPositionX = 0f + (dotIndex * dot.getDiameter() * MARGIN_BETWEEN_DOTS_FACTOR)
+        val endPositionX = calculateEndX(container, dot, dotIndex)
 
         ValueAnimator.ofFloat(startPositionX , endPositionX).apply {
             addUpdateListener { animation ->
                 val currentValue = animation.animatedValue as Float
                 dot.translationX = currentValue
             }
-            duration = 1000L
+            duration = 1000
             repeatMode = ValueAnimator.REVERSE
             repeatCount = ValueAnimator.INFINITE
             start()
         }
+    }
+
+    private fun calculateEndX(container: DotLoading, dot: Dot, dotIndex: Int): Float {
+        return container.getSizeInPixels().width -
+                (dot.getDiameter() * ((container.childCount) - dotIndex) * MARGIN_BETWEEN_DOTS_FACTOR)
     }
 }
